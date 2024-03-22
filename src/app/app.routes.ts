53512e4916c9b96f3e6df9e339fb5a7bd9a97979
Felipe Quérette <felipe.querette@gmail.com>
2024-03-22 17:32:25 -0300

    Added sidebar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 export const routes: Routes = [
     {
@@ -19,6 +20,10 @@ export const routes: Routes = [
     {
         path: "cadastro",
         component: CadastroComponent,
+    },
+    {
+        path: "sidebar",
+        component: SidebarComponent,
     },
 
 ];
