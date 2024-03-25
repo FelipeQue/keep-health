9aa62db14a20fdd1f59f5ab97ea072852678f738
Felipe Quérette <felipe.querette@gmail.com>
2024-03-25 01:25:22 -0300

    DietDetail implemented.

--- src/app/diet/diet-detail/diet-detail.component.ts
+++ src/app/diet/diet-detail/diet-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-diet-detail',
@@ -13,7 +14,7 @@ export class DietDetailComponent {
   foodName: string = "";
   food: any = {};
 
-  constructor(private activatedRoute: ActivatedRoute, private router: Router){};
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private location: Location){};
 
   getStorage(storageName: string) {
     const storage = localStorage.getItem(storageName);
@@ -27,24 +28,16 @@ export class DietDetailComponent {
 
   ngOnInit(){
     this.activatedRoute.params.subscribe((parameters)=>{
-      // console.log(parameters);
-      // this.foodName = parameters.id; <- esse não funciona.
       this.foodName = parameters['id'];
     });
-    
   let listOfFoods = this.getStorage("foodList");
-
   let searchedFoods = listOfFoods.filter((searchedFood: { name: string | undefined; }) => searchedFood.name == this.foodName);
-
   this.food = searchedFoods[0];
-
-  
-
-
-
-
   };
 
+  goBack() {
+  this.location.back();
+  }
 
 }
 
